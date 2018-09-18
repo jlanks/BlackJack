a49940dd5cd3ac360325007aa0f8a2bca3e1ee43
Julian Lankstead <Julian@dhcp-153-38.cu-wireless-students.carleton.ca>
2018-09-18 11:31:36 -0400

    test: created tests for the Deck class.

--- src/test/java/core/BlackJackTest.java
+++ src/test/java/core/BlackJackTest.java
@@ -3,7 +3,9 @@ package core;
 import junit.framework.TestCase;
 
 public class BlackJackTest extends TestCase{
-	
+	//========================================
+	//TESTS FOR THE CARD CLASS 
+	//========================================
 	public void test_getCNum() {
 		
 		Card threeClubs = new Card(Suit.C,3);
@@ -31,4 +33,95 @@ public class BlackJackTest extends TestCase{
 		assertEquals("3 of C",threeClubs.toString()) ; 
 		
 		}
+	//========================================
+	//BIG TESTS FOR THE DECK CLASS 
+	//========================================
+	/*
+	public void test_ForAllCards() {
+		//Create a Deck
+		
+		Deck JackBlack = new Deck(false); 
+		
+		//First check if the size of the deck is correct
+		assertEquals(52,JackBlack.getSize()); 
+		
+		//loop that cycles through deck with checks ->
+		//to see if every card of every suit is there
+		
+		//Contains what the Current card in the deck should be
+		Card CurrentC; 
+		
+		// loops through suits 
+		
+		for(int j=0;j<4;j++) {
+			
+		// loops through numbers
+			
+			for(int i =1;i<14;i++) {
+			
+			 CurrentC = new Card(Suit.values()[j],i); 
+			 Card DeckCard = JackBlack.getTopCard(); 
+				
+			 	assertEquals(CurrentC.getCNum() ,DeckCard.getCNum()); 
+			 	assertEquals(CurrentC.getSuit() ,DeckCard.getSuit());
+			}
+		}
+	}
+	
+	*/
+	//========================================
+	//SMALLER TESTS FOR THE DECK CLASS 
+	//========================================
+	
+	//making sure Card is being assigned correct values
+	
+	public void test_CorrectCardAssignment() {
+		// i = suit      j = card number 
+		int i, j;   i=0;   j=3; 
+		
+		Card CurrentC; 
+		
+		//assigning card variables 
+		CurrentC = new Card(Suit.values()[i],j); 
+		
+		//testing assignment was a success 
+		assertEquals(3,CurrentC.getCNum());
+		assertEquals(Suit.C,CurrentC.getSuit());
+		
+	}
+	
+	//testing to ensure decks are being created properly
+	
+	public void test_DeckCreation() {
+		
+		//creating a deck, not shuffled
+		Deck JackBlack = new Deck(false);
+		
+		Card CurrentC; 
+		
+		//making the 3 of clubs
+		CurrentC = JackBlack.get(0);  
+		
+		//testing to see if the first card is correct 
+		assertEquals(1,CurrentC.getCNum());
+		assertEquals(Suit.C,CurrentC.getSuit());
+		
+		}
+	
+	
+	
+	
+	
+	//public void test_Shuffle() {}
+	
+	
+	
+	/*public void test_SupportFileInput()
+	public void test_SupportConsoleInput()
+	public void test_OfferInput()
+	*/
+	
+	
+	
+	
 }
