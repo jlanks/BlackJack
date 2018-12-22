82c8cabd353e8e023c800c8f7f9f5919b90ade14
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2018-12-22 16:05:04 -0500

    HandTest: test added for the indexOfAce

--- src/test/java/core/HandTest.java
+++ src/test/java/core/HandTest.java
@@ -47,5 +47,28 @@ public class HandTest {
 		
 		
 	}
+	
+	@Test
+	public void Indexof_Test() {
+		
+		Hand hand = new Hand(); 
+		
+		Card c1 = new Card(Suit.C,10);
+		Card c2 = new Card(Suit.D,10);
+		Card c3 = new Card(Suit.D,1);
+		
+		hand.add(c1);
+		hand.add(c2);
+		hand.add(c3);
+		hand.add(c3);
+		
+		assertEquals(4,hand.size());
+		assertEquals(2,hand.indexOfAce().length);
+		assertEquals(22,hand.getSum());
+		System.out.println(hand.toString());
+		
+		
+		
+	}
 
 }
